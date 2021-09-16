17ad5500feb7d68c6e4ce469fe3e0334a730c95a
TanyaSummer <89299874440@mail.ru>
2021-09-16 22:55:53 +0300

    add slick slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
-  // $('body').hide();
+  
+  $(".star").rateYo({
+    numStars: 5,
+    starWidth: "18px",
+    ratedFill:'#E7DE1B'
+  });
+  
+  $('.product-items__inner').slick({
+    // arrows:true,
+    dots:true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // autoplay: true,
+    // autoplaySpeed:3000,
+  });
+
 });
